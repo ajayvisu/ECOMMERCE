2ee57b8f77064b5a4fd5ea4626a4692346dff534
ajayvisu <103246613+ajayvisu@users.noreply.github.com>
2022-05-13 13:31:03 +0530

    Add files via upload

--- app.js
+++ app.js
@@ -1,26 +1,31 @@
 const express           = require("express");
-const app               = express();
+const cors              = require("cors");
 const mongoose          = require("mongoose");
 const dotenv            = require("dotenv");
+const morgan            = require("morgan");
 
 const userRouter        = require("./routes/user.route");
 const authRouter        = require("./routes/auth.route");
 const productRouter     = require("./routes/product.route");
 const cartRouter        = require("./routes/cart.route");
 
-dotenv.config();
+const app = express();
+require('dotenv').config();
 
-app.listen(process.env.PORT || 6000, ()=>{
-    console.log("backend server running successfully!");
+app.listen(process.env.PORT || 8000, ()=>{
+    console.log("backend server running successfully! on port 8000");
 });
 
 mongoose.connect(process.env.DB_URL, {useNewUrlParser : true, useUnifiedTopology : true})
         .then(()=>{console.log("DB connected successfully!");})
-        .catch((err)=>{console.log(err.message);});
+        .catch((err)=>{console.log(err.message);
+});
 
 app.use(express.json());
-app.use("/api/users", userRouter);
+app.use(cors());
+
 app.use("/api/auth", authRouter);
+app.use("/api/users", userRouter);
 app.use("/api/products", productRouter);
 app.use("/api/carts", cartRouter);
 
